9b6fcc657ddebaed469db16a1dc2146de35ca4de
ADEPU SAI KIRAN <145094365+SaiKiran-Adepu@users.noreply.github.com>
2025-10-08 17:02:21 +0530

    YB-1212-Select_Your_NoteBook

---
+++ pages/notebook_fuctionality_page.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+from core.base_page import BasePage
+
+class NotebookPage(BasePage):
+    COMPUTERS_MENU     = (By.LINK_TEXT, "Computers")
+    NOTEBOOKS_MENU      = (By.LINK_TEXT, "Notebooks")
+    SELECT_NOTEBOOK     = (By.LINK_TEXT, "14.1-inch Laptop")
+    ADD_TO_CART_BUTTON = (By.ID, "add-to-cart-button-31")
+    SHOPPING_CART_LINK = (By.XPATH, "//*[@id='topcartlink']/a/span[1]")#//*[@id="flyout-cart"]
+
+    def __init__(self, driver, env):
+        super().__init__(driver, env)
+
+    def open_computers_menu(self):
+        self.click(self.COMPUTERS_MENU)
+
+    def open_notebooks(self):
+        self.click(self.NOTEBOOKS_MENU)
+
+    def select_notebook(self):
+        self.click(self.SELECT_NOTEBOOK)
+
+    def add_to_cart(self):
+        self.click(self.ADD_TO_CART_BUTTON)
+
+    def view_shopping_cart(self):
+        self.click(self.SHOPPING_CART_LINK)
---
+++ tests/test_notebook_fuctionality.py
@@ -0,0 +1,19 @@
+import allure
+import pytest
+from pages.notebook_fuctionality_page import NotebookPage
+from pages.home_page import HomePage
+
+class TestNotebooksSelection:
+    @allure.story("Select Your Notebook")
+    @pytest.mark.smoke
+    def test_notebook_functionality(self, driver, env):
+        hp = HomePage(driver, env)
+        hp.open_home()
+
+        np = NotebookPage(driver, env)
+        np.open_computers_menu()
+        np.open_notebooks()
+        np.select_notebook()
+        np.add_to_cart()
+        np.view_shopping_cart()
+        assert "Demo Web Shop. Shopping Cart" in driver.title #confirmation

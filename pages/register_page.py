77fd33c3bb7945001447af18a9b09de40fc20470
Sriram362 <sriramreddymula616@gmail.com>
2025-10-15 23:55:19 +0530

    YW-54-T5_register-match the password and confirm password

--- pages/register_page.py
+++ pages/register_page.py
@@ -9,6 +9,23 @@ class RegisterPage(BasePage):
     PASSWORD      = (By.ID, "pass")
     CONFIRM_PWD   = (By.ID, "confirm")
     REGISTER_BTN  = (By.ID, "submit")
+    #Error validation
+    PASSWORD_MISMATCH =(By.ID,"confirmErr")
+    #SMOKE VALIDATION
+    REGISTER_PAGE_TITLE =(By.XPATH,'//*[@id="h1]')
+    FIRSTNAME_FIELD_TITLE =(By.XPATH,'//*[@id="form"]/div[1]/div[1]/label')
+    FIRSTNAME_FIELD_WATER_MARK =(By.XPATH,'//*[@id="first"]')
+    LASTNAME_FIELD_TITLE =(By.XPATH,'//*[@id="form"]/div[1]/div[2]/label')
+    LASTNAME_FIELD_WATER_MARK =(By.XPATH,'//*[@id="last"]')
+    EMAIL_FIELD_TITLE=(By.XPATH,'//*[@id="form"]/div[1]/div[3]/label')
+    EMAIL_FIELD_WATER_MARK=(By.XPATH,'//*[@id="email"]')
+    PASSWORD_FIELD_TITLE=(By.XPATH,'//*[@id="form"]/div[1]/div[4]/label')
+    PASSWORD_FIELD_WATER_MARK=(By.XPATH,'//*[@id="pass"]')
+    CONFIRM_PWD_FIELD_TITLE=(By.XPATH,'//*[@id="form"]/div[1]/div[5]/label')
+    CONFIRM_PWD_FIELD_WATER_MARK=(By.XPATH,'//*[@id="confirm"]')
+    REGISTER_BUTTON=(By.XPATH,'//*[@id="submit"]')
+    SIGIN_OPTION=(By.XPATH,'//*[@id="form"]/div[2]/a')
+    FOOTER_LINE=(By.XPATH,'/html/body/main/div[2]/span[1]')
 
     def __init__(self, driver, env):
         super().__init__(driver, env)
@@ -24,3 +41,9 @@ class RegisterPage(BasePage):
     def enter_password(self, v):         self.type(self.PASSWORD, v)
     def enter_confirm_password(self, v): self.type(self.CONFIRM_PWD, v)
     def click_register_button(self):     self.click(self.REGISTER_BTN)
+    #def if_password_error_displayed(self):  self.is_visible(self.PASSWORD_MISMATCH)
+    def password_error(self):   return self.text_of(self.PASSWORD_MISMATCH)
+
+
+
+
--- tests/test_register.py
+++ tests/test_register.py
@@ -1,3 +1,5 @@
+from dbm import error
+
 import allure
 import pytest, uuid
 from pages.home_page import HomePage
@@ -30,3 +32,38 @@ class TestRegister:
 
         lp = LoginPage(driver, env)
         assert_equals(lp.welcome_message(), "Welcome Back")
+
+    @allure.story("YW-T5-Verify_Password_and_Confirm_Password")
+    @pytest.mark.funtional
+    @pytest.mark.yw_t5
+    @pytest.mark.parametrize("data", test_data)
+    def test_verify_password_and_confirm_password(self, driver, data, env):
+        hp = HomePage(driver, env)
+        hp.open_home()
+
+        rp = RegisterPage(driver, env)
+        rp.open_register_page()
+        rp.enter_first_name(data["FirstName"])
+        rp.enter_last_name(data["LastName"])
+        rp.enter_email_address(data["Email"])
+        rp.enter_password(data["Password"])
+        rp.enter_confirm_password(data["IncorrectPassword"])
+        rp.click_register_button()
+
+        rp.wait_visible(rp.PASSWORD_MISMATCH)
+        error_text=rp.password_error()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
